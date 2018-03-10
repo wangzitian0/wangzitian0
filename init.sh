c1f6dcda00716113863bbfca0f41f8846b407e7a
wangzitian0 <wangzitian0@gmail.com>
2018-03-10 21:01:08 +0800

    refactor blog

---
+++ init.sh
@@ -0,0 +1,6 @@
+mkdir themes/next
+git clone https://github.com/theme-next/hexo-theme-next themes/next
+git clone https://github.com/theme-next/theme-next-algolia-instant-search source/lib/algolia-instant-search
+
+
+export HEXO_ALGOLIA_INDEXING_KEY=6a2714c5b665823e7132db2aedb81233
