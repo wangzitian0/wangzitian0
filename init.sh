5318eb2ce941884dd3e24aacbc788f9716eacd84
wangzitian0 <wangzitian0@gmail.com>
2018-03-12 14:14:57 +0800

    add counter

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 mkdir themes/next
 git clone https://github.com/theme-next/hexo-theme-next themes/next
 git clone https://github.com/theme-next/theme-next-algolia-instant-search source/lib/algolia-instant-search
+git clone https://github.com/theme-next/theme-next-han source/lib/Han
 
 
 export HEXO_ALGOLIA_INDEXING_KEY=6a2714c5b665823e7132db2aedb81233
 
 # /home/zitian/ftest/wangzitian0/themes/next/layout/_third-party/comments/gitment.swig    id: window.location.pathname   ->  id: '{{ page.date }}',
 # https://draveness.me/git-comments-initialize
+
+# https://github.com/theme-next/hexo-leancloud-counter-security
